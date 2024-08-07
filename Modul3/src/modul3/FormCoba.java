5f782d021da957acced66f7786b1d9c4076b46a4
Asihharyanti <151506365+Asihharyanti@users.noreply.github.com>
2024-08-07 14:39:59 +0700

    tugas pemrograman visual pertemuan 1-4

---
+++ Modul3/src/modul3/FormCoba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class FormCoba {
+
+    public FormCoba() {
+    }
+    
+}
---
+++ modul1/src/modul1/Percobaan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class Percobaan {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
