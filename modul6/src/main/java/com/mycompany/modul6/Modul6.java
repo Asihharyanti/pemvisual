c1bf36dbd4b3571c49b4455d09650ae035b63f5b
Asihharyanti <151506365+Asihharyanti@users.noreply.github.com>
2024-09-04 15:55:13 +0700

    Add files via upload

---
+++ modul6/src/main/java/com/mycompany/modul6/Modul6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modul6;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
